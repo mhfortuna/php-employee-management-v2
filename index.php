af2b1629fbfa107851053c3f1f2c33f07504dba8
Mathias Fortuna <mathiasfortuna@hotmail.com>
2021-07-26 11:08:21 +0200

    Changed location of classes

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 require_once 'config/baseConstants.php';
 require_once 'config/constants.php';
 require_once 'config/db.php';
-require_once LIBS . '/Database.php';
-require_once LIBS . '/Controller.php';
-require_once LIBS . '/View.php';
-require_once LIBS . '/Model.php';
+require_once LIBS . '/classes/Database.php';
+require_once LIBS . '/classes/Controller.php';
+require_once LIBS . '/classes/View.php';
+require_once LIBS . '/classes/Model.php';
+require_once LIBS . '/classes/SessionHelper.php';
 require_once LIBS . '/Router.php';
-require_once LIBS . '/SessionHelper.php';
 
 $router = new Router();
